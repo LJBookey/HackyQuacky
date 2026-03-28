f9cf8bd0bdae190fd648e8eb56efdf362fd7eb8f
Gemma Watson <gemmamwatson@gmail.com>
2026-03-28 19:28:39 +0000

    implamented the webm to wav function

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ Talk to ducks.
 pip install flask 
 pip install flask-cors
 pip install debug 
+
+# for webm to wav 
+sudo apt-get install -y python-pydub
+sudo apt-get install ffmpeg 
--- src/server/main.py
+++ src/server/main.py
@@ -3,7 +3,7 @@ from fileinput import filename
 from flask import *  
 from flask_cors import CORS
 import os
-
+from pydub import AudioSegment
 
 app = Flask(__name__)
 CORS(app)
@@ -24,10 +24,15 @@ def uploadAudio():
   target_path = os.path.join(UPLOAD_FOLDER, filename)
   audio_file.save(target_path)
 
+  convertFromWebmToWav(target_path)
+
   # I think we can put the stuff for translating here 
   print ("Ducky say: The problem is in the code")
   return jsonify({"status": "ok", "response": "Ducky say: The problem is in the code"})
 
+def convertFromWebmToWav(target_path):
+  sound = AudioSegment.from_file(target_path)
+  sound.export(os.path.join(UPLOAD_FOLDER, "Audio.wav"), format="wav")
 
 if __name__ == '__main__':  
     app.run()
