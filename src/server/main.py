be27b3313291e44284720e09b6a4a70c55af3687
Lucas Bookey <lb365@st-andrews.ac.uk>
2026-03-29 08:09:37 +0100

    its crucnh rime

--- src/server/main.py
+++ src/server/main.py
@@ -1,36 +1,43 @@
 #from distutils.log import debug
 from fileinput import filename
-from flask import *  
+from flask import *
 from flask_cors import CORS
 import os
 from pydub import AudioSegment
+
+import threading
+
 
 import csv
 
 app = Flask(__name__)
 CORS(app)
 
-UPLOAD_FOLDER = "uploads"
+UPLOAD_FOLDER = "../"
 
 @app.route('/')  
 def main():  
+    print("asdasd")
     return render_template("translate.html")  
+
+
 
 @app.route('/upload/audio', methods = ['POST'])  
 def uploadAudio():
 
   audio_file = request.files.get('audio_data')
   filename = "myAudioFile.webm"
-  
-  target_path = os.path.join(UPLOAD_FOLDER, filename)
-  audio_file.save(target_path)
 
-  convertFromWebmToWav(target_path)
+  target_path = os.path.join(UPLOAD_FOLDER, filename)  # Adjust the path as needed
+  audio_file.save(target_path)
+  
+
+  # convertFromWebmToWav(target_path)
 
   # I think we can put the stuff for translating here 
   response = numbersToMessage([1,2], "English")
 
-  print ("Ducky say: The problem is in the code")
+  print("Ducky say: The problem is in the code")
   return jsonify(response)
 
 def convertFromWebmToWav(target_path):
@@ -69,4 +76,4 @@ def load_translations(filename="src/server/translations.csv"):
 data = load_translations()
 
 if __name__ == '__main__':  
-    app.run()
+    app.run(use_reloader=False, debug=False, threaded=True)
